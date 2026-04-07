73efd01738f07371a3279c0db43c2166e2cf1ef5
marcosdev <marcos.herreros@des.kyocera.com>
2026-04-07 12:55:18 +0200

    Validaciones de modelo probadas y funcionando en Swagger

--- backend/TareasAPI/TareasAPI/Models/Tarea.cs
+++ backend/TareasAPI/TareasAPI/Models/Tarea.cs
@@ -1,13 +1,26 @@
+using System.ComponentModel.DataAnnotations; // Necesario para las validaciones []
+
 namespace TareasAPI.Models
 {
     public class Tarea
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
         public string Titulo { get; set; } = string.Empty;
+
+        [MaxLength(500, ErrorMessage = "La descripción es demasiado larga")] 
         public string Descripcion { get; set; } = string.Empty;
+
+        [Required]
         public EstadoTarea Estado { get; set; }
+
+        [Required]
         public PrioridadTarea Prioridad { get; set; }
+
         public DateTime FechaCreacion { get; set; }
+
         public DateTime FechaLimite { get; set; }
     }
 
@@ -19,6 +32,7 @@ namespace TareasAPI.Models
         Completada
     }
 
+    // Enum que define los niveles de prioridad posibles de una tarea
     public enum PrioridadTarea
     {
         Baja,
