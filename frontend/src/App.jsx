daae4ba89bc0a5474f05d748021c2f84f7e6592b
Andrei <havier.pamfile@des.kyocera.com>
2026-04-07 11:20:22 +0200

    Inicializar proyecto de React para el frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Gestor de Tareas 📋</h1>
+      <p>¡Hola desde el Frontend de Andrei!</p>
+    </div>
+  )
+}
+
+export default App
