0376ba75f23b2407ad7b514fed8ce9241187f58b
marcosdev <marcos.herreros@des.kyocera.com>
2026-04-08 09:51:48 +0200

    Fix: Restaurado DTO de paginación y sincronizado con main

--- backend/TareasAPI/TareasAPI/Dtos/RespuestaPaginadaDto.cs
+++ backend/TareasAPI/TareasAPI/Dtos/RespuestaPaginadaDto.cs
@@ -1,11 +1,21 @@
-namespace TareasAPI.Dtos
+using System;
+using System.Collections.Generic;
+
+namespace TareasAPI.Models
 {
     public class RespuestaPaginadaDto<T>
     {
+        // La lista de datos (tareas, usuarios, etc.)
         public IEnumerable<T> Datos { get; set; } = new List<T>();
-        public int PaginaActual { get; set; }
-        public int TamañoPagina { get; set; }
+
+        // Metadatos de la paginación
         public int TotalRegistros { get; set; }
+        public int PaginaActual { get; set; }
         public int TotalPaginas { get; set; }
+        public int TamañoPagina { get; set; }
+
+        // Propiedades calculadas para ayudar al Front (compañero)
+        public bool TienePaginaAnterior => PaginaActual > 1;
+        public bool TienePaginaSiguiente => PaginaActual < TotalPaginas;
     }
 }
